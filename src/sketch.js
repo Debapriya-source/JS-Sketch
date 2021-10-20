5f5193122a869b7b60ee0c2d512ebd15616ef9cd
DEBAPRIYA DAS <debapriyadas73@gmail.com>
2021-10-20 12:04:36 +0530

    ..

--- src/sketch.js
+++ src/sketch.js
@@ -1,16 +1,16 @@
 
 function setup() {
-    createCanvas(windowWidth, windowHeight);
+    createCanvas(750, 400);
   }
 
   function draw() {
     background(0);
-    strokeWeight(4);
+    strokeWeight(2);
     stroke(255);
-    for(let x= 0;x<=width;x+=50) {
-      for (let y = 20; y < height; y+=50) {    
-      fill(255,0,random(255));
-      ellipse(x, y, 25, 25);
+    for(let x= 25;x<=mouseX;x+=50) {
+      for (let y = 25; y < mouseY; y+=50) {    
+      fill(random(255),0,random(255));
+      ellipse(x, y, 15, 15);
       }
     }
   }
