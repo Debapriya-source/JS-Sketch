1baa14d44ef0f95146cd448926d05a387786619b
DEBAPRIYA DAS <debapriyadas73@gmail.com>
2021-10-20 13:47:10 +0530

    ..

--- src/sketch.js
+++ src/sketch.js
@@ -1,16 +1,36 @@
-
+let b = [];
 function setup() {
-    createCanvas(windowWidth,windowHeight);
+  createCanvas(windowWidth, windowHeight);
+}
+function mouseDragged() {
+  let r = random(10, 70);
+  let newBubble = new Bubble(mouseX, mouseY, r);
+  b.push(newBubble);
+}
+function draw() {
+  background(0);
+  for (let i = 0; i < b.length; i++) {
+    b[i].move();
+    b[i].show();
   }
+}
 
-  function draw() {
-    background(0);
-    strokeWeight(2);
-    stroke(255);
-    for(let x= 25;x<=mouseX;x+=50) {
-      for (let y = 25; y < mouseY; y+=50) {    
-      fill(random(255),0,random(255));
-      ellipse(x, y, 15, 15);
-      }
-    }
+class Bubble {
+  constructor(x, y, radius) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
   }
+  show() {
+    // stroke(255, 200);
+    // strokeWeight(3);
+    // noFill();
+    noStroke();
+    fill(255,50);
+    ellipse(this.x, this.y, this.radius, this.radius);
+  }
+  move() {
+    this.x = this.x + random(-5, 5);
+    this.y = this.y + random(-7, 5);
+  }
+}
